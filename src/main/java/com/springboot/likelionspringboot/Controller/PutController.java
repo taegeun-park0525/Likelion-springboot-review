be751b303496320113a31b543c3c922ba87384c4
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-27 21:36:09 +0900

    [add] PutController

---
+++ src/main/java/com/springboot/likelionspringboot/Controller/PutController.java
@@ -0,0 +1,28 @@
+package com.springboot.likelionspringboot.Controller;
+
+import com.springboot.likelionspringboot.domain.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+
+    @PutMapping(value = "/member1")
+    public String putMemberDto1( MemberDto memberDto) {
+        return memberDto.toString();
+    }
+
+    @PutMapping(value = "/member2")
+    public MemberDto putMemberDto2( MemberDto memberDto) {
+        return memberDto;
+    }
+
+    @PutMapping("/member3")
+    public ResponseEntity<MemberDto> putMemberDto3(MemberDto memberDto) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
+    }
+}
