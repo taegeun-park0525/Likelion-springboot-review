218daa732b3398df2625a8c9545bec9a7d43200f
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-29 17:37:03 +0900

    [@Slf4j] @Slf4j로 사용자 임의 로그 남기기

--- src/main/java/com/springboot/likelionspringboot/Controller/PutController.java
+++ src/main/java/com/springboot/likelionspringboot/Controller/PutController.java
@@ -1,16 +1,19 @@
 package com.springboot.likelionspringboot.Controller;
 
 import com.springboot.likelionspringboot.domain.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/put-api")
+@Slf4j
 public class PutController {
 
     @PutMapping(value = "/member1")
     public String putMemberDto1( MemberDto memberDto) {
+        log.info("PutController : member1으로 요청이 들어왔습니다.");
         return memberDto.toString();
     }
 
